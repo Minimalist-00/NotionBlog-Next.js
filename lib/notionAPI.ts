c9dad2b1b5f427a65549c8b6cfc4230e777d837d
Minimalist-00 <kouki4122@icloud.com>
2023-05-08 14:42:53 +0900

    All change

---
+++ lib/notionAPI.ts
@@ -0,0 +1,13 @@
+import { Client } from "@notionhq/client";
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export const getAllPosts = async () => {
+  const posts = await notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID,
+  });
+  const allPosts = posts.results;
+  return allPosts;
+};
